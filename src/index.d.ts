ba98888c182594110b18f3dfc809de24b1840963
Paolo Insogna <paolo@cowtech.it>
2020-10-24 10:02:23 +0200

    feat: Use exact enumeration for TokenError codes.

--- src/index.d.ts
+++ src/index.d.ts
@@ -18,7 +18,23 @@ export type Algorithm =
 
 declare class TokenError extends Error {
   static wrap(originalError: Error, code: string, message: string): TokenError
-  static codes: { [key: string]: string }
+  static codes: {
+    invalidType: 'FAST_JWT_INVALID_TYPE'
+    invalidOption: 'FAST_JWT_INVALID_OPTION'
+    invalidAlgorithm: 'FAST_JWT_INVALID_ALGORITHM'
+    invalidClaimType: 'FAST_JWT_INVALID_CLAIM_TYPE'
+    invalidClaimValue: 'FAST_JWT_INVALID_CLAIM_VALUE'
+    invalidKey: 'FAST_JWT_INVALID_KEY'
+    invalidSignature: 'FAST_JWT_INVALID_SIGNATURE'
+    invalidPayload: 'FAST_JWT_INVALID_PAYLOAD'
+    malformed: 'FAST_JWT_MALFORMED'
+    inactive: 'FAST_JWT_INACTIVE'
+    expired: 'FAST_JWT_EXPIRED'
+    missingKey: 'FAST_JWT_MISSING_KEY'
+    keyFetchingError: 'FAST_JWT_KEY_FETCHING_ERROR'
+    signError: 'FAST_JWT_SIGN_ERROR'
+    verifyError: 'FAST_JWT_VERIFY_ERROR'
+  }
 
   code: string;
   [key: string]: any
