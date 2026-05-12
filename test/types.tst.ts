c3c337946add33f8207e3f9080394484af519c37
Luca Del Puppo <luca@delpuppo.net>
2026-05-12 16:25:58 +0200

    refactor(tests): update CI test command and remove unused tsconfig

--- test/types.tst.ts
+++ test/types.tst.ts
@@ -1,5 +1,4 @@
-/* eslint-disable @typescript-eslint/no-unused-expressions */
-/* eslint-disable @typescript-eslint/no-unused-vars */
+/// <reference types="node" />
 
 import { expect } from 'tstyche'
 import {
